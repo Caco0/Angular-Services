4ba082dc3abd8101d04d555c5ee8a628c9d6ce9a
Rafael Dametto <cacotpds@gmail.com>
2023-11-29 09:42:42 -0300

    Injecting Service

--- Angular-Services/src/app/components/card/card.component.ts
+++ Angular-Services/src/app/components/card/card.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
-
-  constructor() { }
+  name: string = 'Charmander';
+  atributeTypes: string[] = ['Fire', 'Rock'];
+  constructor(private service: PokemonService) {}
 
   ngOnInit(): void {
+    this.service.getPokemon('pikachu');
   }
-
 }
---
+++ Angular-Services/src/app/services/pokemon.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokemonService {
+  constructor() {}
+
+  getPokemon(pokemonName: string) {
+    console.log(pokemonName);
+  }
+}
