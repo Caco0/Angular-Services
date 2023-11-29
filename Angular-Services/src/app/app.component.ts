8fdfb9d2ac50d3468b72ed357adbc394e7c40940
Rafael Dametto <cacotpds@gmail.com>
2023-11-29 08:17:31 -0300

    Inserting Component Card and Styling

--- Angular-Services/src/app/app.component.ts
+++ Angular-Services/src/app/app.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: ` <h1>Aula de Services</h1>
+    <app-card></app-card>
+    <router-outlet></router-outlet>`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Angular-Services';
